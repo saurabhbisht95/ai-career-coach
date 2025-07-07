7eda64b6a31fcc1d356d970c7e923b8e1cd41bbd
saurabhbisht95 <bishtsaurabh79@gmail.com>
2025-07-07 23:17:17 +0530

    added the features section

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,9 +1,33 @@
+"use client"
+
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Button } from "./ui/button";
 import Image from "next/image";
 
 const HeroSection = () => {
+
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const imageElement = imageRef.current;
+
+    const handleScroll = () =>{
+    const scrollPosition = window.scrollY;
+    const scrollThreshold = 100;
+
+    if(scrollPosition > scrollThreshold){
+      imageElement.classList.add("scrolled")
+    } else{
+      imageElement.classList.remove("scrolled")
+    }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+
   return (
     <section className="w-full pt-36 md:pt-48 pb-10">
       <div className="space-y-6 text-center">
@@ -32,8 +56,8 @@ const HeroSection = () => {
           </Link>
         </div>
 
-        <div>
-            <div>
+        <div className="hero-image-wrapper  mt-5 md:mt-0">
+            <div ref={imageRef} className="hero-image">
                <Image src={"/banner.jpeg"}
                 width={1280}
                 height={720}
