7cc3f6ea6a2d451397020466c18aa006f5dfda6f
Saurabh <bishtsaurabh15@gmail.com>
2025-07-07 18:27:51 +0530

    fixed the hero Section scroll issue

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
       >
         <ThemeProvider
             attribute="class"
-             forcedTheme="dark"
+            forcedTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <div>
       <div className="grid-background">
+        </div>
         <HeroSection />
-      </div>
+      
     </div>
   );
 }
